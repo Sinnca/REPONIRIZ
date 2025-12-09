bf0fa2656c81b094269ab2b34fe8154171d02c90
johnkkvnsn <0323-3554@lspu.edu.ph>
2025-12-09 18:00:30 +0800

    admin/statistics.php assets/css/admin_statistics.css

--- admin/statistics.php
+++ admin/statistics.php
@@ -86,6 +86,8 @@ $topUsers = $stmtTopUsers->fetchAll();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Statistics - Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../assets/css/admin_statistics.css?v=1">
+
     <style>
         .content-wrapper {
             padding: 30px;
