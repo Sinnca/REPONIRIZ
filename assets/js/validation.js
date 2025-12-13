4c00dd59e00b917781f95c37f6ea190747163bc1
Rui Manalo <ruicourse3@gmail.com>
2025-12-13 12:50:50 +0800

    fix the date cannot be in the future kapag today ang pinili

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -26,11 +26,14 @@ const Validator = {
     /**
      * Check if date is not in future
      */
-    isNotFutureDate(date) {
-        const inputDate = new Date(date);
-        const today = new Date();
-        today.setHours(0, 0, 0, 0);
-        return inputDate <= today;
+   isNotFutureDate(date) {
+    const inputDate = new Date(date);
+    inputDate.setHours(0, 0, 0, 0);
+
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    return inputDate <= today;
     },
 
     /**
